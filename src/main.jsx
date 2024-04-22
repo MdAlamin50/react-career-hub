22cfde29f69ab97c42fe82041ffce21ebdcd9e68
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-04-23 04:40:51 +0600

    added the nave home and error page

--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,23 @@ import "./index.css";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Root from './Components/Root/Root';
 import Home from "./Components/Home/Home";
+import AppliedJobs from "./Components/AppliedJobs/AppliedJobs";
+import ErrorPage from "./Components/ErrorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element:<Root></Root>,
+    errorElement:<ErrorPage></ErrorPage>,
     children: [
       {
         path: '/',
         element:<Home></Home>
       },
+      {
+        path:'/applied',
+        element:<AppliedJobs></AppliedJobs>
+      }
     ],
   },
 ]);
