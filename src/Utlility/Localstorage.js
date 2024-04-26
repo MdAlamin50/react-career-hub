a8bc68af63fa2ef9e2b3b7a53bc97ffe7d93bbaa
Md Alamin50 <mdalaminsk0101@gmail.com>
2024-04-26 23:47:40 +0600

    added the last commit

--- src/Utlility/Localstorage.js
+++ src/Utlility/Localstorage.js
@@ -12,7 +12,7 @@ const storedJobApplication = getStoredJobApplication();
 const exists = storedJobApplication.find(jobId => jobId === id);
 if(!exists){
     storedJobApplication.push(id);
-    localStorage.setItem('job-application', JSON.stringify(storedJobApplication))
+    localStorage.setItem('job-application', JSON.stringify(storedJobApplication)) 
 }
 
 }
